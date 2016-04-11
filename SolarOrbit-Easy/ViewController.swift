080367da7314f135ac9e7e3a9301034e9568e242
GabrielMassana <gabrielmassana@gmail.com>
2016-04-11 13:41:17 +0100

    first investigations about distances

--- SolarOrbit-Easy/ViewController.swift
+++ SolarOrbit-Easy/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
+import PureLayout
+
 class ViewController: UIViewController {
 
+    //MARK: - Accessors
+
+    var SunDiameter: CGFloat = 109.3 // 1392684.0 Km
+    var EarthDiameter: CGFloat = 1.0 //12742.0 Km
+
+    var EarthDistanceFromSun: CGFloat = 11740.7 //149600000 Km
+    
+    var sunImageView: UIImageView = {
+       
+        let sunImageView: UIImageView = UIImageView.newAutoLayoutView()
+        
+        sunImageView.image = UIImage(named: "sun")
+        
+        return sunImageView
+    }()
+    
+    var earthImageView: UIImageView = {
+        
+        let earthImageView: UIImageView = UIImageView.newAutoLayoutView()
+        
+        earthImageView.image = UIImage(named: "earth")
+        
+        return earthImageView
+    }()
+    
+    //MARK: - ViewLifeCycle
+
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(sunImageView)
+        view.addSubview(earthImageView)
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - Constraints
+    
+    override func updateViewConstraints() {
+        
+        super.updateViewConstraints()
+        
+        sunImageView.autoAlignAxisToSuperviewAxis(ALAxis.Horizontal)
+        sunImageView.autoAlignAxisToSuperviewAxis(ALAxis.Vertical)
+        sunImageView.autoSetDimensionsToSize(CGSize.init(width: SunDiameter, height: SunDiameter))
+        
+        earthImageView.autoAlignAxisToSuperviewAxis(ALAxis.Vertical)
+        earthImageView.autoSetDimensionsToSize(CGSize.init(width: EarthDiameter * 30, height: EarthDiameter * 30))
+        earthImageView.autoPinEdge(ALEdge.Bottom, toEdge: ALEdge.Top, ofView: sunImageView, withOffset: -150)
     }
-
-
 }
 
